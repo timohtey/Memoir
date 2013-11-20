53e2c986e070a29ff3f81ee530982a5a18831326
Timothy Rodriguez <timohtey.rodriguez136@gmail.com>
2013-11-20 23:52:36 +0800

    Added LinkPhaseUtility (SUCKS THO)

--- src/com/example/memoir/LinkPhaseUtility.java
+++ src/com/example/memoir/LinkPhaseUtility.java
@@ -1,14 +1,26 @@
 package com.example.memoir;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnKeyListener;
+import android.widget.EditText;
 
 public class LinkPhaseUtility extends Activity {
+	EditText customWordsTxt;
+	
 	protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.linkphase_utility);
         
+        ActionBar actionBar = getActionBar();
+	    actionBar.hide();
+	    
+	    customWordsTxt = (EditText) findViewById(R.id.customWordsTxt);
      }
 
 
