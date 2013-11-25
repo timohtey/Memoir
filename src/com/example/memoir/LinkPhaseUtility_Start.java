5a5b5d5fda58404207dfa5cd45744de29867d82d
Timothy Rodriguez <timohtey.rodriguez136@gmail.com>
2013-11-25 23:23:04 +0800

    Added Delete Feature in LinkPhaseUtility

--- src/com/example/memoir/LinkPhaseUtility_Start.java
+++ src/com/example/memoir/LinkPhaseUtility_Start.java
@@ -1,71 +1,99 @@
 package com.example.memoir;
 
 import java.util.ArrayList;
-
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.ScrollView;
+import android.widget.ListView;
 import android.widget.TextView;
 
-public class LinkPhaseUtility_Start extends Activity{
-	private LinearLayout innerLayout;
-	private ArrayList<TextView> textViews = new ArrayList<TextView>();
-	private Button newBtn;
-	private Button deleteBtn;
-	
-	protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.linkphase_utility_start);
-        
-        ActionBar actionBar = this.getActionBar();
-        actionBar.hide();
-        
-        innerLayout =  (LinearLayout) findViewById(R.id.linearLayout);
-        
-        newBtn = (Button)findViewById(R.id.newWordBtn);
-        deleteBtn = (Button)findViewById(R.id.deleteWordBtn);
-        
-        for(int i = 0; i <10; i++){
-        	textViews.add(new TextView(this));
-        	if(i == 0){
-        		textViews.get(i).setText("hello, hi, here, tata");
-        	}
-        	else if (i == 1){
-        		textViews.get(i).setText("haha, popo, chacha, hihi");
-        	}
-        	else textViews.get(i).setText("blah, blue, green");
-        	textViews.get(i).setTextSize(40);
-        	textViews.get(i).setClickable(true);
-        	textViews.get(i).setOnClickListener(new View.OnClickListener(){
-				public void onClick(View arg0) {
-					// TODO Auto-generated method stub
-					
+public class LinkPhaseUtility_Start	extends Activity {
+		ArrayAdapter<String> mAdapter;
+	    private ArrayList<String> arrayList = new ArrayList<String>();
+		private Button newBtn;
+		private ListView lv;
+		private TextView title;
+		
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.linkphase_utility_start);
+	        
+	        Typeface myTypeface = Typeface.createFromAsset(getAssets(), "fonts/Canter/Canter Bold.otf");
+	        lv = (ListView) findViewById(R.id.list);
+	        //lv = getListView();
+	        newBtn = (Button)findViewById(R.id.newWordBtn);
+	        title = (TextView)findViewById(R.id.title);
+	        title.setTypeface(myTypeface);
+	        
+	        ActionBar actionBar = this.getActionBar();
+	        actionBar.hide();
+	        // Set up ListView example
+	        for(int i = 0; i <10; i++){
+	        	if(i %2== 0){
+	        		arrayList.add("hello, hi, here, tata");
+	        	}
+	        	else {
+	        		arrayList.add("blah, blue, green");
+	        	}
+	        }
+
+	        mAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, arrayList);
+	        lv.setAdapter(mAdapter);  
+	       
+	        // Create a ListView-specific touch listener. ListViews are given special treatment because
+	        // by default they handle touches for their list items... i.e. they're in charge of drawing
+	        // the pressed state (the list selector), handling list item clicks, etc.
+	        SwipeDismissListViewTouchListener touchListener =
+	                new SwipeDismissListViewTouchListener(
+	                        lv,
+	                        new SwipeDismissListViewTouchListener.DismissCallbacks() {
+	                            @Override
+	                            public boolean canDismiss(int position) {
+	                                return true;
+	                            }
+
+	                            @Override
+	                            public void onDismiss(ListView lv, int[] reverseSortedPositions) {
+	                                for (int position : reverseSortedPositions) {
+	                                    mAdapter.remove(mAdapter.getItem(position));
+	                                }
+	                                mAdapter.notifyDataSetChanged();
+	                            }
+	                        });
+	        lv.setOnTouchListener(touchListener);
+	        // Setting this scroll listener is required to ensure that during ListView scrolling,
+	        // we don't look for swipes.
+	        lv.setOnScrollListener(touchListener.makeScrollListener());
+	        
+	        lv.setOnItemClickListener(new OnItemClickListener()
+	        {
+	            @Override
+	            public void onItemClick(AdapterView<?> adapter, View v, int position, long arg3){
+	                  int positionIndex = (Integer) adapter.getItemAtPosition(position); 
+	            }
+	         });
+	        
+	        newBtn.setOnClickListener(new OnClickListener(){    
+	            @Override
+				public void onClick(View v) {
+	            	Intent myIntent = new Intent(LinkPhaseUtility_Start.this, LinkPhaseUtility.class);
+	                startActivity(myIntent);
 				}
-        	});
-        		  
-        	innerLayout.addView(textViews.get(i));
-        }
-        
-        newBtn.setOnClickListener(new OnClickListener(){    
-            @Override
-			public void onClick(View v) {
-            	Intent myIntent = new Intent(LinkPhaseUtility_Start.this, LinkPhaseUtility.class);
-                startActivity(myIntent);
-			}
-        }); 
-        
-	}
-	
-	public boolean onCreateOptionsMenu(Menu menu) {
-	    // Inflate the menu; this adds items to the action bar if it is present.
-	    //getMenuInflater().inflate(R.menu.main, menu);
-	    return true;
-	}
+	        }); 
+	    }
+
+	    
+	  /* protected void onListItemClick(ListView lv, View view, int position, long id) {
+	        Toast.makeText(this,
+	                "Clicked " + getListAdapter().getItem(position).toString(),
+	                Toast.LENGTH_SHORT).show();
+	    }*/
 }
