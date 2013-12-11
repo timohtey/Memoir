08a34a4efdf08143f7d5203984973c7932611a1e
lordd466b <lordd466b@gmail.com>
2013-12-12 05:46:06 +0800

    Added Route Link Phase Screen

---
+++ src/com/example/memoir/RouteLinkPhase.java
@@ -0,0 +1,33 @@
+package com.example.memoir;
+
+import android.os.Bundle;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+
+public class RouteLinkPhase extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_route_link_phase);
+		
+		ActionBar actionBar = getActionBar();
+	    actionBar.hide();
+		
+		
+		Button yourBtn = (Button) findViewById(R.id.rl_backBtn);
+		int btnSize=yourBtn.getLayoutParams().width;
+		yourBtn.setLayoutParams(new LayoutParams(btnSize, btnSize));
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.route_link_phase, menu);
+		return true;
+	}
+
+}
